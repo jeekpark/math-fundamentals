f58a69408611b7c0b3d3f87d01cd46b087355710
jeekpark <jeekpark@student.42seoul.kr>
2023-12-15 19:33:24 +0900

    add: Vector3D.hpp

---
+++ Matrix3D.hpp
@@ -0,0 +1,58 @@
+/**
+ * @file Matrix3D.hpp
+ * @author Jeekun Park (jeekunp@naver.com)
+ * @brief 
+ * @version 0.1
+ * @date 2023-12-15
+ * 
+ * @copyright Copyright (c) 2023
+ * 
+ */
+
+
+#pragma once
+
+#include "Vector3D.hpp"
+
+struct Matrix3D
+{
+public:
+  Matrix3D() = default;
+  Matrix3D(float n00, float n01, float n02,
+           float n10, float n11, float n12,
+           float n20, float n21, float n22)
+  {
+    n[0][0] = n00; n[0][1] = n10; n[0][2] = n20;
+    n[1][0] = n01; n[1][1] = n11; n[1][2] = n21;
+    n[2][0] = n02; n[2][1] = n12; n[2][2] = n22;
+  }
+
+  Matrix3D(const Vector3D& a, const Vector3D& b, const Vector3D& c)
+  {
+    n[0][0] = a.x; n[0][1] = a.y; n[0][2] = a.z;
+    n[1][0] = b.x; n[1][1] = b.y; n[1][2] = b.z;
+    n[2][0] = c.x; n[2][1] = c.y; n[2][2] = c.z;
+  }
+
+  float& operator()(int i, int j)
+  {
+    return n[j][i];
+  }
+
+  const float& operator()(int i, int j) const
+  {
+    return n[j][i];
+  }
+
+  Vector3D& operator[](int j)
+  {
+    return *reinterpret_cast<Vector3D *>(n[j]);
+  }
+
+  const Vector3D& operator[](int j) const
+  {
+    return *reinterpret_cast<const Vector3D *>(n[j]);
+  }
+private:
+  float n[3][3];
+};
